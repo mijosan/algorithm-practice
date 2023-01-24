9e6bb67bffcabb6e70fd001ed4fdd8dc398751f0
ChoiTaesan <mijosan@naver.com>
2023-01-24 15:01:40 +0900

    fix: 크기가 작은 부분 문자열 코드 개선

--- programmers/lv1/Solution.java
+++ programmers/lv1/Solution.java
@@ -3,30 +3,13 @@ package lv1;
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-
         int pLength = p.length();
+        long pValue = Long.parseLong(p);
 
-        int[] arr = new int[t.length()];
+        for (int i = 0; i <= t.length() - pLength; i++) {
+            long tValue = Long.parseLong(t.substring(i, i + pLength));
 
-        for (int i = 0; i < t.length(); i++) {
-            arr[i] = t.charAt(i) - '0';
-        }
-
-        for (int i = 0; i < t.length(); i++) {
-            if (arr.length - pLength < i) {
-                break;
-            }
-
-            StringBuffer sb = new StringBuffer();
-
-            int count = 0;
-            for (int j = i; count < pLength; j++) {
-
-                sb.append(arr[j]);
-                count++;
-            }
-
-            if (Long.parseLong(p) >= Long.parseLong(sb.toString())) {
+            if (tValue <= pValue) {
                 answer++;
             }
         }
@@ -37,9 +20,9 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        // System.out.println(solution.solution("3141592", "271"));
+        System.out.println(solution.solution("3141592", "271"));
         System.out.println(solution.solution("88888888888888888888888888888888888888888888", "777777777777777777"));
-        // System.out.println(solution.solution("10203", "15"));
+        System.out.println(solution.solution("10203", "15"));
     }
 
 }
